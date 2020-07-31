f1f6727f0eb13dc4453aa14e900693c335f7b44e
Joe Dougherty <joseph.dougherty@gmail.com>
2020-07-31 15:14:36 -0400

    tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,29 +1,38 @@
 from buildaspider import Spider
 
 
-BASIC_CONFIG = 'test_config/basic.ini' 
+basic_spider = Spider('test_config/basic.ini')
 
-myspider = Spider(BASIC_CONFIG)
-
-myspider.weave()
+basic_spider.weave()
 
 #
 # In this example, we are restricting the spider to this one URL
 #
 def test_single_url_crawl_stopped_did_not_wander_off():
     assert(
-        myspider.visited_urls == {'http://crawler-test.com/links/broken_links_internal'}
+        basic_spider.visited_urls == {'http://crawler-test.com/links/broken_links_internal'}
     )
 
 
 def test_single_url_found_all_broken_links():
     # http://crawler-test.com/links/broken_links_internal
     assert(
-        myspider.broken_urls == {
+        basic_spider.broken_urls == {
             'http://crawler-test.com/links/not_found/foo4', 
             'http://crawler-test.com/links/not_found/foo1', 
             'http://crawler-test.com/links/not_found/foo2', 
             'http://crawler-test.com/links/not_found/foo3', 
             'http://crawler-test.com/links/not_found/foo5',
         }
+    )
+
+
+basic_spider_external_links = Spider('test_config/basic2.ini')
+
+basic_spider_external_links.weave()
+
+
+def test_single_url_crawl_stopped_did_not_wander_off_external_links():
+    assert(
+        basic_spider_external_links.visited_urls == {'http://crawler-test.com/links/page_with_external_links'}
     )
